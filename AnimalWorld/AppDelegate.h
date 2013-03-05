4a2c9845685d6403a8025bb4328454717fb6adc8
IOS12 <ios12@MAC10.local>
2013-03-05 20:48:21 +0700

    Initial Commit

---
+++ AnimalWorld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AnimalWorld
+//
+//  Created by IOS12 on 3/5/13.
+//  Copyright (c) 2013 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AnimalWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnimalWorld
+//
+//  Created by IOS12 on 3/5/13.
+//  Copyright (c) 2013 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
